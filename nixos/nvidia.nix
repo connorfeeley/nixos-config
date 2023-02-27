e9589df22a5911447211d82eed15da95c91582d1
Connor Feeley <git@cfeeley.org>
2023-02-27 13:27:48 -0500

    nixos: fix nvidia

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -4,12 +4,33 @@
 
 { config, pkgs, lib, ... }:
 let
-  nvidia_x11 = config.boot.kernelPackages.nvidia_x11;
+  nvidia_x11 = nvStable.nvidia_x11;
   nvidia_gl = nvidia_x11.out;
   nvidia_gl_32 = nvidia_x11.lib32;
+
+  nvStable = pkgs.nur.repos.arc.packages.nvidia-patch.override {
+    nvidia_x11 = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 in
 # Nvidia hates ARM
-lib.mkIf (pkgs.stdenv.is64bit && pkgs.stdenv.isx86_64) {
+{
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    package = nvStable;
+    modesetting.enable = false;
+    nvidiaSettings = true; # Enable nvidia-settings utility
+    nvidiaPersistenced = false; # Don't run daemon to keep GPU state alive
+    # Prevent display corruption upon wake from a suspended or hibernated state.
+    powerManagement.enable = true;
+
+    # "Whether to force-enable the full composition pipeline. This sometimes fixes
+    # screen tearing issues. This has been reported to reduce the performance of
+    # some OpenGL applications and may produce issues in WebGL. It also drastically
+    # increases the time the driver needs to clock down after load."
+    forceFullCompositionPipeline = false;
+  };
+
   hardware.opengl = {
     enable = true;
 
