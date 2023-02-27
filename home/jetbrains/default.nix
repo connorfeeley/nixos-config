42646fb29eec8edd6d7b201af694e7ccb89c7e91
Connor Feeley <git@cfeeley.org>
2023-02-27 11:29:45 -0500

    home: enable jetbrains on darwin too

--- home/jetbrains/default.nix
+++ home/jetbrains/default.nix
@@ -3,13 +3,14 @@
 # SPDX-License-Identifier: MIT
 
 { config, lib, pkgs, ... }: {
-  home.packages = lib.mkIf pkgs.stdenv.isx86_64 (with pkgs.jetbrains; [
-    clion
-    datagrip
-    gateway
-    goland
-    idea-ultimate
-    pycharm-professional
-    webstorm
-  ]);
+  home.packages = lib.mkIf (with pkgs.stdenv; (isLinux && isx86_64) || (isDarwin))
+    (with pkgs.jetbrains; [
+      clion
+      datagrip
+      gateway
+      goland
+      idea-ultimate
+      pycharm-professional
+      webstorm
+    ]);
 }
