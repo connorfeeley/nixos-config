c59eb00af3d6189e41ed50ffbf2b2f07c8c34a70
Connor Feeley <git@cfeeley.org>
2023-02-26 20:58:10 -0500

    nixos: fix 32-bit compat

--- nixos/desktopish/steam.nix
+++ nixos/desktopish/steam.nix
@@ -2,11 +2,8 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   environment.systemPackages = with pkgs; [
     steam
   ];
-
-  # https://github.com/NixOS/nixpkgs/issues/47932#issuecomment-447508411
-  hardware.opengl.driSupport32Bit = true;
 }
